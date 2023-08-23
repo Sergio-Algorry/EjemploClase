c69b06d8266fc7a80491988eb70ec359e755b05b
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2023-08-23 09:56:54 -0300

    Agregar archivos de proyecto.

---
+++ EjemploClase/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace EjemploClase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblEt1 = new System.Windows.Forms.Label();
+            this.lblEt2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btAceptar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(46, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(197, 38);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // lblEt1
+            // 
+            this.lblEt1.AutoSize = true;
+            this.lblEt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEt1.Location = new System.Drawing.Point(40, 7);
+            this.lblEt1.Name = "lblEt1";
+            this.lblEt1.Size = new System.Drawing.Size(110, 31);
+            this.lblEt1.TabIndex = 1;
+            this.lblEt1.Text = "Nombre";
+            // 
+            // lblEt2
+            // 
+            this.lblEt2.AutoSize = true;
+            this.lblEt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEt2.Location = new System.Drawing.Point(252, 7);
+            this.lblEt2.Name = "lblEt2";
+            this.lblEt2.Size = new System.Drawing.Size(110, 31);
+            this.lblEt2.TabIndex = 3;
+            this.lblEt2.Text = "Apellido";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(249, 41);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(208, 38);
+            this.txtApellido.TabIndex = 2;
+            // 
+            // btAceptar
+            // 
+            this.btAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAceptar.Location = new System.Drawing.Point(463, 41);
+            this.btAceptar.Name = "btAceptar";
+            this.btAceptar.Size = new System.Drawing.Size(140, 40);
+            this.btAceptar.TabIndex = 4;
+            this.btAceptar.Text = "Aceptar";
+            this.btAceptar.UseVisualStyleBackColor = true;
+            this.btAceptar.Click += new System.EventHandler(this.btAceptar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(46, 109);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 31);
+            this.lblResultado.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 156);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btAceptar);
+            this.Controls.Add(this.lblEt2);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.lblEt1);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblEt1;
+        private System.Windows.Forms.Label lblEt2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btAceptar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ EjemploClase/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploClase
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btAceptar_Click(object sender, EventArgs e)
+        {
+            int pepe = 10;
+
+            Persona persona1 = new Persona();
+
+            Persona persona2 = new Persona();
+
+            persona1.Nombre = txtNombre.Text;
+            persona1.Apellido= txtApellido.Text;
+
+            lblResultado.Text = 
+                persona1.Apellido + ", " + persona1.Nombre;
+
+        }
+    }
+}
